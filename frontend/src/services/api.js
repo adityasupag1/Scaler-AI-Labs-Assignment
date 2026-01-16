4ed520382d5237e16e44eb7abde684364f9ce01e
Aditya Raj <araj15397@gmail.com>
2026-01-16 15:34:48 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,3 @@
 
-const API_URL = "http://localhost:5000";
+const API_URL = "scaler-ai-labs-assignment.railway.internal" ||  "http://localhost:5000" ;
 export const getBoard = async () => (await fetch(`${API_URL}/board`)).json();
