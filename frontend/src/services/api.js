e761d3e929475413e2af85a13f54ddbbb325b4cf
Aditya Raj <araj15397@gmail.com>
2026-01-16 17:12:02 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 const API_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://scaler-ai-labs-assignment-eh5l.vercel.app/";
+    : "https://scaler-ai-labs-assignment-eh5l.vercel.app";
 
 export const getBoard = async () =>
   (await fetch(`${API_URL}/board`, { credentials: "include" })).json();
