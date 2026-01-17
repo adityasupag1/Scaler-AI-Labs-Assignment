a2e06b747b28e64869d3c3fbe4544113e9bd7f5a
Aditya Raj <araj15397@gmail.com>
2026-01-17 10:04:01 +0530

    Update BoardPage.jsx

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -3,7 +3,7 @@ import Board from "../components/Board";
 export default function BoardPage() {
   return (
     <div className="p-4 md:p-6">
-      <h1 className="text-2xl font-bold mb-4">Scaler Project Board</h1>
+      <h1 className="text-2xl font-bold mb-4 text-center">Scaler Project Board</h1>
       <Board />
     </div>
   );
