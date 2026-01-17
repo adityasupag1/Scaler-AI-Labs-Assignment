5285df68724c8f9c6f3752c6417905bc7006d016
Aditya Raj <araj15397@gmail.com>
2026-01-17 05:43:44 +0530

    Create BoardPage.jsx

---
+++ src/pages/BoardPage.jsx
@@ -0,0 +1,10 @@
+import Board from "../components/Board";
+
+export default function BoardPage() {
+  return (
+    <div className="p-4 md:p-6">
+      <h1 className="text-2xl font-bold mb-4">Scaler Project Board</h1>
+      <Board />
+    </div>
+  );
+}
